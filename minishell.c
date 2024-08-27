96f7a8eb8f3bfab5dd14320ed8f8bbf90216b7e4
dramos-n <dramos-n@student.42malaga.com>
2024-08-27 18:20:12 +0200

    Created structs

--- minishell.c
+++ minishell.c
@@ -4,3 +4,7 @@ int	main(int argc, char **argv, char **env)
 {
 
 }
+
+//1-	cmd
+//2-	cmd | cmd | cmd
+//3-	</> file cmd | cmd
--- minishell.h
+++ minishell.h
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum s_type
+{
+	PIPE,
+	COMMAND,
+	IN,
+	INN,
+	OUT,
+	OUTT
+}
+
 typedef struct s_env
 {
 	char			*key;
 	char			*value;
 	struct s_env	*next;
-}				t_env;
+}					t_env;
+
+typedef struct s_redir
+{
+	char			*name;
+	int				type;
+	struct s_redir	*next;
+}					t_redir;
+
+typedef struct s_cmd
+{
+	char			**cmd;
+	int				type;
+	struct s_redir	redir;
+	struct s_cmd	*prev;
+	struct s_cmd	*next;
+}					t_cmd;
